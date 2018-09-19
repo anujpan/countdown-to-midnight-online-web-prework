c5fd8113cd0a580b0a2f9354659b3ae0a553a30c
Anuj Panchal <anujpan1@gmail.com>
2018-09-19 22:25:55 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
 def countdown(number)
-  value = number
-  while value > 0
-    puts "#{value} SECOND(S)!" 
-    value -= 1
+  time = number
+  while time > 0
+    puts "#{time} SECOND(S)!" 
+    time -= 1
   end
   
   "HAPPY NEW YEAR!"
